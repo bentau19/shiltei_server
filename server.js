f17b5359167356f6fe427aed07720ebec2c919c3
bentau19 <bentau19@gmail.com>
2023-09-21 14:09:41 +0300

    TEST

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ async function connect(){
         //     console.log('server on port 8000')
         // })
         app.listen(process.env.PORT || 3000, () => {
+            console.log("chagned")
             console.log(`Server running on port ${process.env.PORT || 3000}`);
         });
         console.log("Connected to mongoDB");
